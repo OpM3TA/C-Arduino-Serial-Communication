b227f42852f3d6c2f7211ad76248fb3c5c8b19ce
OpM3TA <xmodfree@gmail.com>
2015-05-25 01:22:13 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,42 @@
+// SerialComm.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <tchar.h>
+#include "SerialClass.h"
+#include <string>
+
+// application reads from the specified serial port and reports the collected data
+
+// Woo, thanks for the Serial Class Arduino.cc!
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("Welcome to the serial test app!\n\n");
+
+	Serial* SP = new Serial("\\\\.\\COM10");    // adjust as needed
+
+	if (SP->IsConnected())
+		printf("We're connected");
+
+	char incomingData[256] = "";			// don't forget to pre-allocate memory
+	//printf("%s\n",incomingData);
+	int dataLength = 256;
+	int readResult = 0;
+
+	while (SP->IsConnected())
+	{
+		readResult = SP->ReadData(incomingData, dataLength);
+		printf("Bytes read: (-1 means no data available) %i\n", readResult);
+		
+		std::string test(incomingData);
+
+		printf("%s", incomingData);
+		test = "";
+
+		Sleep(500);
+	}
+	return 0;
+}
+
+
